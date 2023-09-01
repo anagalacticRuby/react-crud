9128588120c5c7cd7d276ffac0f6aa0d88d3ab6e
anagalacticRuby <nick12.aded@gmail.com>
2023-09-01 15:11:33 -0400

    Added CRUD functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+
+import { useState, useEffect } from "react";
 
 function App() {
+  const ENDPOINT = "https://638959874eccb986e8905258.mockapi.io/CrudAPI/Hotel"; //Endpoint for Hotel Resource
+  const [reservations, setReservations] = useState([{}]); //A default value can be declared in case the reservations variable is never updated.
+
+  //Variables used for the post method
+  const [newReservationName, setNewReservationName] = useState("");
+  const [newRoomNumber, setNewRoomNumber] = useState("");
+  const [newStayLength, setNewStayLength] = useState("");
+
+  //Variables for the update method
+  const [updatedReservationName, setUpdatedReservationName] = useState("");
+  const [updatedRoomNumber, setUpdatedRoomNumber] = useState("");
+  const [updatedStayLength, setUpdatedStayLength] = useState("");
+
+  function getReservation() {
+    fetch(ENDPOINT)
+      .then((data) => data.json())
+      .then((data) => setReservations(data));
+  }
+
+  useEffect(() => {
+    getReservation();
+    console.log(reservations);
+  }, []);
+
+  function deleteReservation(id) {
+    fetch(`${ENDPOINT}/${id}`, {
+      method: "DELETE",
+    }).then(() => getReservation());
+  }
+
+  function postReservation(e) {
+    e.preventDefault();
+    console.log(newReservationName, newRoomNumber, newStayLength);
+
+    fetch(ENDPOINT, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        reservationName: newReservationName,
+        roomNumber: newRoomNumber,
+        stayLength: newStayLength,
+      }),
+    }).then(() => getReservation());
+  }
+
+  function updateReservation(e, reservationObj) {
+    e.preventDefault();
+
+    let updatedReservationObj = {
+      ...reservationObj,
+      reservationName: updatedReservationName,
+      roomNumber: updatedRoomNumber,
+      stayLength: updatedStayLength,
+    };
+
+    fetch(`${ENDPOINT}/${reservationObj.id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(updatedReservationObj),
+    }).then(() => getReservation());
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Create a New Reservation</h3>
+      <form>
+        <label>Reservation Name </label>
+        <input
+          type="text"
+          placeholder="Reservation Name"
+          onChange={(e) => setNewReservationName(e.target.value)}
+        />{" "}
+        <br />
+        <label>Room Number </label>
+        <input
+          type="number"
+          placeholder="Room Number"
+          onChange={(e) => setNewRoomNumber(e.target.value)}
+        />{" "}
+        <br />
+        <label>Stay Length</label>
+        <input
+          type="number"
+          placeholder="Length of Stay"
+          onChange={(e) => setNewStayLength(e.target.value)}
+        />{" "}
+        <br />
+        <button onClick={(e) => postReservation(e)}>Add Reservation</button>
+      </form>
+
+      {reservations.map((reservation, index) => (
+        <div className="reservationContainer" key={index}>
+          <div>
+            Reservation Name: {reservation.reservationName} <br />
+            Room Number: {reservation.roomNumber} <br />
+            Stay Length: {reservation.stayLength} <br />
+            <button onClick={() => deleteReservation(reservation.id)}>
+              Delete
+            </button>
+          </div>
+          <form className="updateForm">
+            <h3>Update This Reservation</h3>
+            <label>Update Reservation Name</label>
+            <input
+              placeholder="New Reservation Name"
+              onChange={(e) => setUpdatedReservationName(e.target.value)}
+            />{" "}
+            <br />
+            <label>Update Room Number</label>
+            <input
+              placeholder="New Room Number"
+              onChange={(e) => setUpdatedRoomNumber(e.target.value)}
+            />{" "}
+            <br />
+            <label>Update Stay Length</label>
+            <input
+              placeholder="New Stay Length"
+              onChange={(e) => setUpdatedStayLength(e.target.value)}
+            />
+            <br />
+            <button onClick={(e) => updateReservation(e, reservation)}>
+              Update
+            </button>
+          </form>
+        </div>
+      ))}
     </div>
   );
 }
